4495ccaf4b966ad0b3129dd7c82369df2bbb1809
razvan9310 <razvan.damachi@gmail.com>
2014-04-22 22:30:37 +0300

    Adding a simple sliding banner sketch

---
+++ JS/homepage.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#banner-slide").bjqs({
+        animtype: "slide",
+        animspeed: 5000,
+        height: 198,
+        nexttext: '>',
+        prevtext: '<',
+        responsive: true,
+        randomstart: true,
+        width: 1000
+    });
+});
